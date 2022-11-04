adcdd18d44d4550051e88ff2a987d5aa288c5be2
root <ihabasaad1@gmail.com>
2022-11-04 17:36:45 +0000

    add classification metrics

--- datachallenge/evaluation_metrics/classification.py
+++ datachallenge/evaluation_metrics/classification.py
@@ -1,28 +1,34 @@
 from __future__ import absolute_import
 
 from ..utils import to_torch
+from torchmetrics.classification import MulticlassF1Score
+from torchmetrics.functional import precision_recall
+from torchmetrics import Accuracy
 
 
 def accuracy(output, target, topk=(1,)):
-    output, target = to_torch(output), to_torch(target)
-    maxk = max(topk)
-    batch_size = target.size(0)
+    accuracy = Accuracy()
+    return accuracy(output, target)
+    # output, target = to_torch(output), to_torch(target)
+    # maxk = max(topk)
+    # batch_size = target.size(0)
 
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    # _, pred = output.topk(maxk, 1, True, True)
+    # pred = pred.t()
+    # correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-    ret = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(dim=0, keepdim=True)
-        ret.append(correct_k.mul_(1. / batch_size))
-    return ret
+    # ret = []
+    # for k in topk:
+    #     correct_k = correct[:k].view(-1).float().sum(dim=0, keepdim=True)
+    #     ret.append(correct_k.mul_(1. / batch_size))
+    # return ret
+
+def precision_recall(output, target):
+    # prec_, rec_ = precision_recall(preds, target, average='macro', num_classes=8)
+    prec_, rec_ = precision_recall(preds, target, average='macro', num_classes=8) # https://torchmetrics.readthedocs.io/en/stable/classification/precision_recall.html
+    return prec_, rec_
 
 def f1(output, target):
-    return 0
-
-def precision(output, target):
-    return 0
-
-def recall(output, target):
-    return 0
+    metric = MulticlassF1Score(num_classes=8) # https://torchmetrics.readthedocs.io/en/stable/classification/f1_score.html
+    f1_ = metric(output, target)
+    return f1_
